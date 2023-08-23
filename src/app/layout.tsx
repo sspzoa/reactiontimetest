36c1c544217c2a2e87828db383648f23f423a6cd
Seungpyo Suh <107336329+sspzoa@users.noreply.github.com>
2023-08-23 22:31:30 +0900

    error fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import '@/styles/globals.css'
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import styles from "@/styles/Test.module.css";
 
 const inter = Inter({subsets: ['latin']})
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
 import Test from "@/components/Test";
-import About from "@/components/About";
-import Skills from "@/components/Skills";
-import Projects from "@/components/Projects";
-import Contact from "@/components/Contact";
-import Languages from "@/components/Languages";
 
 export default function Home() {
     return (
